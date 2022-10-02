46b284ede52606e8f12a41107e76e2757354fb42
AlexKarumbi <alex.muhia@moringaschool.com>
2022-10-02 20:44:05 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
@@ -6,8 +8,27 @@ import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
+
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route path='/movies' >
+          <Movies/>
+        </Route>
+        <Route path='/directors' >
+          <Directors/>
+        </Route>
+        <Route path='/actors' >
+          <Actors/>
+        </Route>
+        <Route path='/' >
+          <Home/>
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
